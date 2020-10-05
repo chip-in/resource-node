5e4517f388e208ece8bfcf6723c4dd3fbb475654
kkuwa <takuwa@procube.jp>
2020-10-05 13:31:39 +0900

    Update deps

--- examples/browser-node/webpack.config.js
+++ examples/browser-node/webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 var CORE_NODE_URL = "http://core"
 module.exports = {
@@ -14,45 +15,83 @@ module.exports = {
   devtool: 'inline-source-map',
   module: {
     rules: [
+      {
+        test: /\.js$/, exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {"targets": {"node": "current"}}]
+              ]
+            }
+          }
+        ]
+      },
       { test: /\.css$/, loader: ["style-loader", "css-loader"] },
       { test: /\.(jpe?g|png|gif|svg|ico)(\?.+)?$/, loader: "url-loader"}
     ]
   }, 
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env" : {
+        IGNORE_CORENODE_CLUSTERING_ERROR: "true"
+      }
+    })
+  ],
   devServer: {
     contentBase: 'public',
-    proxy: {
+    proxy: [
       /* application */
-      '/a/' : {
+      {
+        context: '/a/' ,
         target: CORE_NODE_URL,
         secure: false
       },
       /* database */
-      '/d/' : {
+      {
+        context: '/d/',
         target: CORE_NODE_URL,
         secure: false
       },
       /* request */
-      '/r/' : {
+      {
+        context: '/r/',
         target: CORE_NODE_URL,
         secure: false,
         ws: true
       },
       /* mqtt */
-      '/m' : {
+      {
+        context: (pathName, req)=>pathName === '/m',
         target: CORE_NODE_URL,
         secure: false,
         ws: true
       },
       /* node registration */
-      '/n/' : {
+      {
+        context: '/n/',
         target: CORE_NODE_URL,
         secure: false
       },
       /* node configuration */
-      '/c/' : {
+      {
+        context: '/c/',
+        target: CORE_NODE_URL,
+        secure: false
+      },
+      /* logging*/
+      {
+        context: '/l/',
+        target: CORE_NODE_URL,
+        secure: false
+      },
+      /* consul */
+      {
+        context: '/v1/',
         target: CORE_NODE_URL,
         secure: false
       }
-    }
+    ]
   }
 };
